5b1dbb2a2548c48f9003684b8225fb6a45c4910b
noufalkdlr <noufalkakdlr@gmail.com>
2025-04-16 20:32:33 +0530

    fetch data

--- netflix-clone/src/pages/Home/Home.jsx
+++ netflix-clone/src/pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Home.css";
 import Navbar from "../../components/Navbar/Navbar";
 import hero_banner from "../../assets/hero_banner.jpg";
@@ -9,17 +9,38 @@ import TitleCards from "../../components/TitleCards/TtitleCards";
 import Footer from "../../components/Footer/Footer";
 
 const Home = () => {
+    const [apiData, setApiData] = useState({});
+
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4ZGFjMDBhY2E0OWJhMjYxYjBkZWFhYWMxNTE0MDc0NyIsIm5iZiI6MTc0NDM0OTAzNC41NjYwMDAyLCJzdWIiOiI2N2Y4YTc2YWQ4MTc0OGNjZmY5OTg3MmQiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.Z3RkAde64wAdwOwNSmjEo3pYKQkxqbI_r3U_JfUxbcI",
+      },
+    };
+
+    useEffect(() => {
+      fetch(
+        'https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1',
+        options
+      )
+        .then((res) => res.json())
+        .then((res) => setApiData(res.results[0]))
+        .catch((err) => console.error(err));
+    }, []);
+
+
   return (
     <div className="home">
       <Navbar />
       <div className="hero">
+        {/* <img src={`https://image.tmdb.org/t/p/w500${apiData?.backdrop_path}`} alt="" className="banner-img" /> */}
         <img src={hero_banner} alt="" className="banner-img" />
         <div className="hero-caption">
           <img src={hero_title} alt="" className="caption-img" />
           <p>
-            dolor sit amet consectetur adipisicing elit. Quidem aperiam
-            consectetur similique debitis dolorum.Lorem, ipsum ur similique
-            debitis dolorum
+              {apiData.overview}
           </p>
           <div className="hero-btns">
             <button className="btn"><img src={play_icon} alt="" />Play</button>
